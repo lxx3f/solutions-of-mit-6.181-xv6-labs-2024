2808c0d70ec78b9ced5a97c92464cbf1431aeae5
lx <2323658175@qq.com>
2025-02-02 00:31:39 +0800

    complete lab4.3 alarm

--- kernel/trap.c
+++ kernel/trap.c
@@ -78,7 +78,20 @@ usertrap(void)
 
   // give up the CPU if this is a timer interrupt.
   if(which_dev == 2)
+  {
+    struct proc *proc = myproc();
+    if(proc->alarm_interval && proc->have_return)
+    {
+      if(++proc->passed_ticks == 2)
+      {
+        proc->saved_trapframe = *p->trapframe;
+        proc->trapframe->epc = proc->handler_va;
+        proc->passed_ticks = 0;
+        proc->have_return = 0;
+      }
+    }
     yield();
+  }
 
   usertrapret();
 }

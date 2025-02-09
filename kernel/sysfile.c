15fa1385bead381803ed091e3bbc68bd219676d9
lx <2323658175@qq.com>
2025-02-09 16:36:34 +0800

    complete lab8.2

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -16,6 +16,8 @@
 #include "file.h"
 #include "fcntl.h"
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
 static int
@@ -328,6 +330,33 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+      int ok = 0;
+      for(int depth = 0; depth < 10; depth++){
+        if(readi(ip, 0, (uint64)path, 0, min(ip->size, MAXPATH)) != ip->size){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        path[ip->size] = '\0';
+        iunlockput(ip);
+        if((ip = namei(path)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        if(ip->type != T_SYMLINK){
+          ok = 1;
+          break;
+        }
+      }
+      if(!ok){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+    }
+
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -501,5 +530,28 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void){
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+  int len;
+  if((len = argstr(0, target, MAXPATH)) < 0 || argstr(1, path, MAXPATH) < 0){
+    return -1;
+  }
+  begin_op();
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)target, 0, len) != len){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
